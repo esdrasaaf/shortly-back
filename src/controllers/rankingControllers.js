1a9f1c68f825e9fe077fab7fd2d45d143e28d752
Esdras Antero <esdrasanterofontes@gmail.com>
2022-12-20 00:29:08 -0300

    feat: ranking route and controller

--- src/controllers/rankingControllers.js
+++ src/controllers/rankingControllers.js
@@ -1,3 +1,11 @@
+import connection from "../database/db.js"
+
 export default async function getRanking (req, res) {
-    res.send("ranking")
+    try {
+        const ranking = await connection.query('SELECT users.id, users.name, COUNT(urls."shortUrl") AS "linksCount", COALESCE(SUM(urls."visitCount"), 0) AS "visitCount" FROM users LEFT JOIN urls ON users.id = urls."userId" GROUP BY users.id ORDER BY "visitCount" DESC, "linksCount" DESC LIMIT 10;');
+        return res.status(200).send(ranking.rows)
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(500)
+    }
 }
--- src/routes/rankingRoutes.js
+++ src/routes/rankingRoutes.js
@@ -3,7 +3,6 @@ import getRanking from "../controllers/rankingControllers.js";
 
 const router = Router();
 
-// precisa usar left join
 router.get("/ranking", getRanking);
 
 export default router;
